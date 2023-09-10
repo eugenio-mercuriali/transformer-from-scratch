a5199c05e300f03eda6b3b9110b5c635beeb4118
eugenio.mercuriali <eugenio.mercuriali@brenntag.com>
2023-09-10 16:23:12 +0200

    add multi-head self-attention block

--- model.py
+++ model.py
@@ -89,6 +89,69 @@ class FeedForwardBlock(nn.Module):
         return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
 
 
+class MultiHeadAttentionBlock(nn.Module):
+    # h is the number of heads
+    def __init__(self, d_model: int, h: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+
+        # d_model should be divisible by h, otherwise we cannot divide equally the same vector
+        # representing the embedding into equal matrices for each head
+        assert d_model % h == 0, 'd_model is not divisible by h'
+
+        self.d_k = d_model // h
+        self.w_q = nn.Linear(d_model, d_model)  # Wq
+        self.w_k = nn.Linear(d_model, d_model)  # Wk
+        self.w_v = nn.Linear(d_model, d_model)  # Wv
+
+        self.w_o = nn.Linear(d_model, d_model)
+        self.dropout = nn.Dropout(dropout)
+        self.attention_scores = None
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]  # taking the last dimension
+
+        # @ is the matrix multiplication in PyTorch
+        # (batch, h, seq_len, d_k) --> (batch, h, seq_len, seq_len)
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            # Replace all the values for which mask == 0 with a very small value
+            # When we apply the softmax, these values will be replaced by 0s
+            attention_scores.masked_fill_(mask == 0, -1e9)
+        attention_scores = attention_scores.softmax(dim=-1)  # (batch, h, seq_len, seq_len)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        # We return the attention scores for visualisation purposes
+        return (attention_scores @ value), attention_scores
+
+    def forward(self, x, q, k, v, mask):
+        query = self.w_q(q)  # shape: (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+        key = self.w_k(k)  # shape: (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+        value = self.w_v(v)  # shape: (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+
+        # We are not splitting the batch or the sequence length, but the embedding
+        # We transpose since we want the h dimension to be the second dimension instead of the third
+        # In this way each head will see all the sentence (sequence length and d_k)
+        # Each head will watch each word of the sentence but a smaller part of the embedding
+
+        # (batch, seq_len, d_model) --> (batch, seq_len, h, d_k) --> (batch, seq_len, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.d_k).transpose(1, 2)
+
+        # We do the same for the key and the value
+        key = key.view(key.shape[0], key.shape[1], self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.d_k).transpose(1, 2)
+
+        x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
+
+        # (batch, h, seq_len, d_k) --> (batch, h, seq_len, d_k) --> (batch, seq_len, d_model)
+        # self.h * self.d_k is equal to d_model
+        x = x.transpose(1, 2).contiguos().view(x.shape[0], -1, self.h * self.d_k)
+
+        # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+        return self.w_o(x)
 
 
 
