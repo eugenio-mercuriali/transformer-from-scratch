587e9cd0eb2f15c48f3a695bb79ba13a457bac5c
eugenio.mercuriali <eugenio.mercuriali@brenntag.com>
2023-09-10 13:44:11 +0200

    add feed-forward block

--- model.py
+++ model.py
@@ -77,6 +77,19 @@ class PositionalEncoding(nn.Module):
             return self.alpha * (x - mean) / (std + self.eps) + self.bias
 
 
+class FeedForwardBlock(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff)  # W1 and B1
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_model, d_ff)  # W2 and B2
+
+    def forward(self, x):
+        # Input sentence
+        # (Batch, seq_len, d_model) --> (Batch, seq_len, d_ff) --> (Batch, seq_len, d_model)
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
+
 
 
 
