1e443916722c3a2df42138391a46a3356d9af689
eugenio.mercuriali <eugenio.mercuriali@brenntag.com>
2023-09-22 16:26:56 +0200

    dataset methods

--- dataset.py
+++ dataset.py
@@ -35,8 +35,60 @@ class BilingualDataset(Dataset):
         dec_input_tokens = self.tokenizer_src.encode(tgt_text).ids
 
         # Now we need to pad the sentence to reach the sequence length
+        enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) - 2  # for SOS and EOS
+        dec_num_padding_tokens = self.seq_len - len(enc_input_tokens) - 1  # only SOS in the decoder
 
+        if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0:
+            raise ValueError('Sentence is too long')
 
+        # Add SOS and EOS to the source text
+        encoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(enc_input_tokens, dtype=torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token] * enc_num_padding_tokens, dtype=torch.int64)
+            ]
+        )
+
+        # Only adding SOS to the decoder input
+        decoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(dec_input_tokens, dtype=torch.int64),
+                torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64)
+            ]
+        )
+
+        # Adding EOS to the label (what we expect as the output from the decoder)
+        label = torch.cat(
+            [
+                torch.tensor(dec_input_tokens, dtype=torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64)
+            ]
+        )
+
+        assert encoder_input.size(0) == self.seq_len
+        assert decoder_input.size(0) == self.seq_len
+        assert label.size(0) == self.seq_len
+
+        # The mask excludes the padding tokens from the self-attention (encoder)
+        # For the decoder, we exclude padding tokens and subsequent tokens (causal mask)
+        return {
+            "encoder_input": encoder_input,  # (seq_len)
+            "decoder_input": decoder_input,  # (seq_len)
+            "encoder_mask": (encoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int(),  # (1, 1, seq_len)
+            "decoder_mask": (decoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int() & causal_mask(decoder_input),  # (1, seq_len) & (seq_len, seq_len)
+            "label": label,  # (seq_len)
+            "src_text": src_text,
+            "tgt_text": tgt_text
+        }
+
+    # This will return all the values above the diagonal
+    def causal_mask(size):
+        mask = torch.triu(torch.ones(1, size, size), diagonal=1).type(torch.int)
+        return mask == 0
 
 
 
